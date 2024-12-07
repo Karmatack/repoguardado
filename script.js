e10e44ec5c9fda1436a1dc7c7996c4b4a1a0794d
Kennedy <systems.kennedy@gmail.com>
2024-12-07 13:48:11 -0500

    Add video player functionality and update course module data with URLs and timestamps

--- script.js
+++ script.js
@@ -3,74 +3,76 @@ const data = {
         {
             "name": "Introducción",
             "classes": [
-                { "id": 1, "title": "Presentación del Curso" },
-                { "id": 2, "title": "¿Qué es Spring?" },
-                { "id": 3, "title": "Instalación del JDK" },
-                { "id": 4, "title": "Instalación del IDE" }
+                { "id": 1, "title": "Presentación del Curso", "url": "https://www.youtube.com/embed/vILhoJQdaoU", "time": "0" },
+                { "id": 2, "title": "¿Qué es Spring?", "url": "https://www.youtube.com/embed/vILhoJQdaoU", "time": "45" },
+                { "id": 3, "title": "Instalación del JDK", "url": "https://www.youtube.com/embed/vILhoJQdaoU", "time": "155" },
+                { "id": 4, "title": "Instalación del IDE", "url": "https://www.youtube.com/embed/vILhoJQdaoU", "time": "231" }
             ]
         },
         {
             "name": "Spring MVC",
             "classes": [
-                { "id": 1, "title": "¿Qué es MVC?" },
-                { "id": 2, "title": "Creación de Proyecto Web con Spring Boot" },
-                { "id": 3, "title": "Creación de Vista y Controlador" },
-                { "id": 4, "title": "Annotation @RequestMapping" },
-                { "id": 5, "title": "El Objeto Model" },
-                { "id": 6, "title": "Directivas de Thymeleaf" },
-                { "id": 7, "title": "Anotación @ModelAttribute" },
-                { "id": 8, "title": "Anotación @RequestParam" },
-                { "id": 9, "title": "Anotación @PathVariable" }
+                { "id": 1, "title": "¿Qué es MVC?", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "0" },
+                { "id": 2, "title": "Creación de Proyecto Web con Spring Boot", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "40" },
+                { "id": 3, "title": "Creación de Vista y Controlador", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "247" },
+                { "id": 4, "title": "Annotation RequestMapping", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "510" },
+                { "id": 5, "title": "El Objeto Model", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "788" },
+                { "id": 6, "title": "Directivas de Thymeleaf", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "951" },
+                { "id": 7, "title": "Anotación ModelAttribute", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "1300" },
+                { "id": 8, "title": "Anotación RequestParam", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "1612" },
+                { "id": 9, "title": "Anotación PathVariable", "url": "https://www.youtube.com/embed/h7exuILFPRM", "time": "2071" }
             ]
         },
         {
             "name": "Inyección de Dependencias",
             "classes": [
-                { "id": 1, "title": "Creación de un Servicio (Acoplado)" },
-                { "id": 2, "title": "Desacoplando Servicio" },
-                { "id": 3, "title": "Anotación Primary" },
-                { "id": 4, "title": "Anotación Qualifier" }
+                { "id": 1, "title": "Creación de un Servicio Acoplado", "url": "https://www.youtube.com/embed/bRtMW9T2rYc", "time": "0" },
+                { "id": 2, "title": "Desacoplando Servicio", "url": "https://www.youtube.com/embed/bRtMW9T2rYc", "time": "220" },
+                { "id": 3, "title": "Anotación Primary", "url": "https://www.youtube.com/embed/bRtMW9T2rYc", "time": "463" },
+                { "id": 4, "title": "Anotación Qualifier", "url": "https://www.youtube.com/embed/bRtMW9T2rYc", "time": "684" }
             ]
         },
         {
             "name": "CRUD Películas",
             "classes": [
-                { "id": 1, "title": "Introducción" },
-                { "id": 2, "title": "Creación de un Nuevo Proyecto" },
-                { "id": 3, "title": "Creación de Entidades con Entity" },
-                { "id": 4, "title": "Acceso a datos con EntityManager" },
-                { "id": 5, "title": "CrudRepository" },
-                { "id": 6, "title": "Consola H2" },
-                { "id": 7, "title": "Controller Película" },
-                { "id": 8, "title": "Maquetando layouts utilizando fragments de Thymeleaf" },
-                { "id": 9, "title": "Agregando Bootstrap" },
-                { "id": 10, "title": "Creando Form Película" },
-                { "id": 11, "title": "Creando Form Película Pt.2" },
-                { "id": 12, "title": "Creando Form Película Pt.3" },
-                { "id": 13, "title": "Validaciones (Server Side)" },
-                { "id": 14, "title": "Agregando Contenedor de Mensajes" },
-                { "id": 15, "title": "Creando Listado Películas" },
-                { "id": 16, "title": "Subida de Imágenes Frontend Pt.1" },
-                { "id": 17, "title": "Subida de Imágenes Backend Pt.2" },
-                { "id": 18, "title": "Editar Película" },
-                { "id": 19, "title": "Agregando Eliminar" },
-                { "id": 20, "title": "Agregando Paginador" }
+                { "id": 1, "title": "Introducción", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "0" },
+                { "id": 2, "title": "Creación de un Nuevo Proyecto", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "56" },
+                { "id": 3, "title": "Creación de Entidades con Entity", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "153" },
+                { "id": 4, "title": "Acceso a datos con EntityManager", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "658" },
+                { "id": 5, "title": "CrudRepository", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "1247" },
+                { "id": 6, "title": "Consola H2", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "1766" },
+                { "id": 7, "title": "Controller Película", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "1875" },
+                { "id": 8, "title": "Maquetando layouts utilizando fragments de Thymeleaf", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "2409" },
+                { "id": 9, "title": "Agregando Bootstrap", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "2573" },
+                { "id": 10, "title": "Creando Form Película", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "2758" },
+                { "id": 11, "title": "Creando Form Película Pt.2", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "3567" },
+                { "id": 12, "title": "Creando Form Película Pt.3", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "4038" },
+                { "id": 13, "title": "Validaciones Server Side", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "5438" },
+                { "id": 14, "title": "Agregando Contenedor de Mensajes", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "5829" },
+                { "id": 15, "title": "Creando Listado Películas", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "6035" },
+                { "id": 16, "title": "Subida de Imágenes Frontend Pt.1", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "6817" },
+                { "id": 17, "title": "Subida de Imágenes Backend Pt.2", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "7294" },
+                { "id": 18, "title": "Editar Película", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "8637" },
+                { "id": 19, "title": "Agregando Eliminar", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "9659" },
+                { "id": 20, "title": "Agregando Paginador", "url": "https://www.youtube.com/embed/WUgMDlFZBO0", "time": "9914" }
             ]
         },
         {
             "name": "Despedida",
             "classes": [
-                { "id": 1, "title": "Despedida" }
+                { "id": 1, "title": "SpringBoot", "url": "https://www.youtube.com/embed/KpB9FfQimVs", "time": "0" }
             ]
         }
     ]
 };
 
+
 const renderModules = (data) => {
     const container = document.getElementById("modules");
+    const videoPlayer = document.getElementById("video-player");
 
-    data.modules.forEach((module, index) => {
-        // Contenedor de módulo
+    data.modules.forEach((module) => {
+        // Crear contenedor de módulo
         const moduleDiv = document.createElement("div");
         moduleDiv.className = "mb-4 bg-blue-700 rounded-lg overflow-hidden";
 
@@ -90,6 +92,12 @@ const renderModules = (data) => {
             const classItem = document.createElement("li");
             classItem.className = "p-2 rounded-lg bg-blue-600 hover:bg-blue-400 cursor-pointer";
             classItem.textContent = `${cls.id}. ${cls.title}`;
+
+            // Evento al hacer clic en una clase
+            classItem.addEventListener("click", () => {
+                videoPlayer.src = `${cls.url}?start=${cls.time}`;
+            });
+
             classList.appendChild(classItem);
         });
 
@@ -97,7 +105,7 @@ const renderModules = (data) => {
         moduleDiv.appendChild(moduleContent);
         container.appendChild(moduleDiv);
 
-        // Evento para expandir/contraer
+        // Expandir/contraer el módulo
         moduleHeader.addEventListener("click", () => {
             const isVisible = !moduleContent.classList.contains("hidden");
             moduleContent.classList.toggle("hidden", isVisible);
@@ -106,5 +114,5 @@ const renderModules = (data) => {
     });
 };
 
-// Renderizar módulos al cargar la página
+// Renderizar los módulos al cargar la página
 document.addEventListener("DOMContentLoaded", () => renderModules(data));
